8c4b68306ff210158032dcbab1bf4ceb910d88f5
Ian <igraham1@lakeheadu.ca>
2022-07-11 21:17:18 -0400

    little message

--- answer001.js
+++ answer001.js
@@ -1,12 +1,10 @@
 let display;
 display = document.getElementById('answer001');
 let answer = 0;
-//** Values do not exceed 4 Mil, sum of even values*/
-for (let i = 1; i < 4000001; i = i + i) 
-{
-    // console.log("counting", i);
-    if (i % 2 === 0 ) {
-        answer = answer + i;  
+for (let i = 1; i < 1000; i = i + 1) {   
+      console.log("counting", i);  
+    if (i % 3 === 0 || i % 5 === 0) {
+        answer = answer + i;
         // console.log("Counts to total", i);
     }
 }
